50008f5ce9038776e182a750a4a24a7cb2e9acc0
pallascodes <pallascodes@gmail.com>
2021-06-10 23:07:21 -0500

    sistema de likes/dislikes arreglado

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -25,16 +25,27 @@
     </a>
     <footer class="flex text-blue-600 mt-3">
       <div class="flex items-center mr-2">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" v-if="(reaction === null || reaction === false) || !isAuthenticated" @click="react(true)">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
         </svg>
-        <span class="text-gray-500 text-xs ml-1">25</span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" viewBox="0 0 20 20" fill="currentColor" v-if="reaction === true && isAuthenticated" @click="react(true)">
+          <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+        </svg>
+        <span class="text-gray-500 text-xs ml-1">
+          {{ getLikes }}
+        </span>
       </div>
       <div class="flex items-center mr-4">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" v-if="(reaction === null || reaction === true) || !isAuthenticated" @click="react(false)">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5" />
         </svg>
-        <span class="text-gray-500 text-xs ml-1">25</span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" viewBox="0 0 20 20" fill="currentColor" v-if="reaction === false && isAuthenticated" @click="react(false)">
+          <path d="M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z" />
+        </svg>
+        <span class="text-gray-500 text-xs ml-1">
+          {{ getDislikes }}
+        </span>
       </div>
       <div class="flex items-center">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -47,8 +58,17 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "Post",
+  data() {
+    return {
+      reaction: null,
+      likes: 0,
+      dislikes: 0
+    }
+  },
   props: {
     post: Object
   },
@@ -66,7 +86,64 @@ export default {
         minutes = '0' + minutes
 
       return `${day} de ${month} del ${year} a las ${hours}:${minutes}`
+    },
+    async react(reaction) {
+      if (this.isAuthenticated) {
+        await axios
+          .post('api/v1/posts/react/', {
+            post: this.post.id,
+            reaction: reaction
+          })
+          .then(res => {
+            console.log(res.data)
+
+            if (this.reaction === null) {
+              this.reaction = reaction
+              if (reaction === true) {
+                this.likes++
+              } else {
+                this.dislikes++
+              }
+            } else if (this.reaction === reaction) {
+              this.reaction = null
+              if (reaction === true) {
+                this.likes--
+              } else {
+                this.dislikes--
+              }
+            } else {
+              this.reaction = reaction
+              if (reaction === true) {
+                this.likes++
+                this.dislikes--
+              } else {
+                this.likes--
+                this.dislikes++
+              }
+            }
+
+          })
+          .catch(error => console.error(error))
+      } else {
+        console.log('autentiquese')
+      }
     }
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated
+    },
+    getLikes() {
+      return this.likes
+    },
+    getDislikes() {
+      return this.dislikes
+    }
+  },
+  mounted() {
+    this.reaction = this.post.get_reaction
+    this.likes = this.post.get_likes
+    this.dislikes = this.post.get_dislikes
   }
 };
 </script>
