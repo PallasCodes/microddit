5fdd95e04035bb883a9da6deef8a9fc3c32c5370
pallascodes <pallascodes@gmail.com>
2021-06-17 10:08:01 -0500

    netlify routing

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import store from "./store";
 import "./assets/tailwind.css";
 import axios from "axios";
 
-//const local = "http://localhost:8000"
+//const local = "http://127.0.0.1:8000"
 const production = 'https://microddit-api.herokuapp.com/'
 
-axios.defaults.baseURL = production
+axios.defaults.baseURL = local
 
 createApp(App).use(store).use(router, axios).mount("#app");
