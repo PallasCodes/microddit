c072d6f62e795c2156d3f2f0fbc24ec2dbfd8ee0
pallascodes <pallascodes@gmail.com>
2024-10-03 10:11:29 -0600

    base url changed

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import store from "./store";
 import "./assets/tailwind.css";
 import axios from "axios";
 
-const local = "http://localhost:8000"
-// const production = ' '
 
-axios.defaults.baseURL = local
+axios.defaults.baseURL = 'https://microddit-api.onrender.com/'
 
 createApp(App).use(store).use(router, axios).mount("#app");
